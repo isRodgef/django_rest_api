d29bea06a7a70fa9d9406d60b5cd4a7a7c046ad1
Rodger <rodgef101@gmail.com>
2022-10-24 22:26:04 +0200

    file upload settings

--- app/settings.py
+++ app/settings.py
@@ -116,3 +116,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# to be removed
+FILE_UPLOAD_HANDLERS = ['django.core.files.uploadhandler.TemporaryFileUploadHandler',]
