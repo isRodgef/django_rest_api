638b03dabb3e824c7925a7b1bcea7def505e6434
Rodger <rodgef101@gmail.com>
2022-10-29 20:58:40 +0200

    updated router

--- transactions_api/urls.py
+++ transactions_api/urls.py
@@ -3,7 +3,8 @@ from transactions_api import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('processFile/', view=views.TranactionsNew.as_view())
+    path('processFile/',view=views.TranactionsNew.as_view()),
+    path('retrieveRows/',view=views.TranactionsNew.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
