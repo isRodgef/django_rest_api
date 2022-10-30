f416f42e7316fffc72f0c837d8b25917addccefb
Rodger <rodgef101@gmail.com>
2022-10-30 22:27:28 +0200

    model and validation

--- transactions_api/models.py
+++ transactions_api/models.py
@@ -1,10 +1,23 @@
 from pyexpat import model
 from unittest.util import _MAX_LENGTH
+from wsgiref.validate import validator
 from django.db import models
-
+from django.core import serializers
+from django.core.exceptions import ValidationError
 # Create your models here.
+
+import datetime
+
+def validate_year(value):
+    if value.year != 2020:
+        raise ValidationError(
+            ('%(value)s does occur in the year 2020'),
+            params={'value': value},
+        )
+
+
 class TransactionModel(models.Model):
-    date = models.DateField()
+    date = models.DateField(validators = [validate_year])
     country = models.CharField(max_length=100)
     transact_type = models.CharField(max_length=8)
     currency = models.CharField(max_length=10)
