a5de5956e3f4c8b2831d2f11c526fd21534aebbc
Rodger <rodgef101@gmail.com>
2022-11-01 22:23:44 +0200

    new url for countries

--- transactions_api/country_repository.py
+++ transactions_api/country_repository.py
@@ -1,11 +1,19 @@
+from collections import defaultdict
+from itertools import count    
+
 import requests
 
 class CountryWrapper():
 
-    def __init__(self,url) -> None:
+    def __init__(self) -> None:
+        url = "https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json"
         r = requests.get(url)
-        self.country_list = r.json() 
-        pass
 
-    def fetch_info(url):
-        return {}
+        country_list = r.json()
+        self.country_list = defaultdict(lambda : False)
+        for json_data in country_list:
+            self.country_list[json_data['Name'].lower()] = json_data['Code'].lower()
+
+
+    def fetch_info(self, country):
+        return self.country_list[country.lower()]
