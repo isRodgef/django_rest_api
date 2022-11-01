ddbed72a16cff773017de8a32ee1a7327231bf72
Rodger <rodgef101@gmail.com>
2022-11-01 22:23:02 +0200

    fecthing of currency function changed

--- transactions_api/currency_repository.py
+++ transactions_api/currency_repository.py
@@ -5,23 +5,28 @@ import datetime
 
 class CurrencyWrapper():
 
-    def __init__(self,currency) -> None:
-        url = "".join(["https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.", currency ,".EUR.SP00.A"])
+    def __init__(self) -> None:
+        self.rate = None 
+        pass
+    
+    def get_rate(self,currency):
+        self.url = "".join(["https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.", currency ,".EUR.SP00.A"])
         params = {
             'format': 'jsondata',
             'detail': 'dataonly',
             'updatedAfter': datetime.datetime.now().date()
         }
-        r = requests.get(url, params)
+        r = requests.get(self.url, params)
         if r.status_code == 200:
             self.rate = r.json()['dataSets'][0]['series']['0:0:0:0:0']['observations']['0'][0]
         else:
             self.rate = 0
+        return self.rate
 
     def convert(self,value):
         return value * self.rate    
 
 
 if __name__ == '__main__':
-    c = CurrencyWrapper('NOK')
+    c = CurrencyWrapper.get_rate('NOK')
     print(c.rate)
