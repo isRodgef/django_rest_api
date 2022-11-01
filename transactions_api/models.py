c27cb03342b3493b25f7344a4b415de2b34f6d10
Rodger <rodgef101@gmail.com>
2022-11-01 22:22:40 +0200

    model update

--- transactions_api/models.py
+++ transactions_api/models.py
@@ -15,11 +15,17 @@ def validate_year(value):
             params={'value': value},
         )
 
+def validate_transact_type(value):
+     if value.lower() not in ('sale', 'purchase'):
+        raise ValidationError(
+            ('%(value)s is not of type sale or purchase'),
+            params={'value': value},
+        )
 
 class TransactionModel(models.Model):
     date = models.DateField(validators = [validate_year])
     country = models.CharField(max_length=100)
-    transact_type = models.CharField(max_length=8)
+    transact_type = models.CharField(max_length=8,validators=[validate_transact_type])
     currency = models.CharField(max_length=10)
     net = models.FloatField()
-    vat = models.FloatField() 
+    vat = models.FloatField()
