2abf7df15a65aa64d6b5a043bedb9956dc2e37f8
Rodger <rodgef101@gmail.com>
2022-11-02 21:52:15 +0200

    added caching

--- transactions_api/country_repository.py
+++ transactions_api/country_repository.py
@@ -1,15 +1,22 @@
 from collections import defaultdict
 from itertools import count    
 
+from app.cache import function_cache
+
 import requests
+
+@function_cache.cache(ttl=10000)
+def get_country_keys():
+    url = "https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json"
+    r = requests.get(url)
+    return r.json()
 
 class CountryWrapper():
 
     def __init__(self) -> None:
-        url = "https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json"
-        r = requests.get(url)
+        
 
-        country_list = r.json()
+        country_list = get_country_keys()
         self.country_list = defaultdict(lambda : False)
         for json_data in country_list:
             self.country_list[json_data['Name'].lower()] = json_data['Code'].lower()
