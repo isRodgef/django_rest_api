d041ed65a5e4e25994413dec6ad031a2a14e3f2b
Rodger <rodgef101@gmail.com>
2022-11-06 21:56:44 +0200

    redis host from env

--- app/cache.py
+++ app/cache.py
@@ -1,5 +1,7 @@
 from redis import StrictRedis
 from redis_cache import RedisCache
+from app.settings import REDIS_HOST
 
-client = StrictRedis(host="127.0.0.1", decode_responses=True)
+
+client = StrictRedis(host=REDIS_HOST, decode_responses=True)
 function_cache = RedisCache(redis_client=client)
